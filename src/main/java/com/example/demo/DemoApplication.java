ee9def149f64d60373fe7a218890a3afa2c013ce
Sadri <ali.sadri@hp.com>
2023-04-08 09:42:10 -0400

    initial commit to local repo

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/KafkaApplicationMultiListener.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class KafkaApplicationMultiListener
+{
+
+    public static void main(String[] args) throws Exception {
+
+        ConfigurableApplicationContext context = SpringApplication.run(KafkaApplicationMultiListener.class, args);
+
+        MessageProducer producer = context.getBean(MessageProducer.class);
+        producer.sendMessages();
+
+        Thread.sleep(5000);
+        context.close();
+    }
+
+    @Bean
+    public MessageProducer MessageProducer() {
+        return new MessageProducer();
+    }
+    @Component
+    public static class MessageProducer {
+
+        @Autowired
+        private KafkaTemplate<String, Object> multiTypeKafkaTemplate;
+
+        @Value(value = "${multi.type.topic.name}")
+        private String multiTypeTopicName;
+
+        public void sendMessages() {
+            multiTypeKafkaTemplate.send(multiTypeTopicName, new Greeting("Greetings", "World!"));
+            multiTypeKafkaTemplate.send(multiTypeTopicName, new Farewell("Farewell", 25));
+            multiTypeKafkaTemplate.send(multiTypeTopicName, "Simple string message");
+        }
+
+    }
+
+}
